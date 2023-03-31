d15ffb475a9c933ccd60c74e42e2c5d26e7ca01a
Speedfire2407 <Faunsatyr@yandex.ru>
2023-03-31 13:31:58 +0500

    Made task4

---
+++ FinalTask/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.Json;
+
+namespace FinalTask
+{
+    [Serializable]
+    class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public Student(string name, string group, DateTime dateOfBirth)
+        {
+            Name = name;
+            Group = group;
+            DateOfBirth = dateOfBirth;
+        }
+
+    }
+    class BinaryFile
+    {
+        const string SetttingsFileName = @"C:\\Students.dat";
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Student[] students = null;
+
+                if (File.Exists(SetttingsFileName))
+                {                   
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    using (var fs = new FileStream(SetttingsFileName, FileMode.OpenOrCreate))
+                    {
+                        students = (Student[])formatter.Deserialize(fs);
+                    }
+
+                    string path = @"C:\\Task4\\";
+                    DirectoryInfo GroupStudent = new DirectoryInfo(path);
+
+                    GroupStudent.Create();
+                    try
+                    {
+                        
+                        for (int i = 0; i < students.Length; i++) {
+                            string newpath = path + students[i].Group +".txt";
+                            if (!File.Exists(newpath)) 
+                            {
+                                using (StreamWriter line = File.CreateText(newpath))
+                                    {
+                                        line.WriteLine(students[i].Name + " " + students[i].DateOfBirth);
+                                    }
+                                
+                            }
+                            else 
+                            {     
+                                using (StreamWriter line = File.AppendText(newpath))
+                                {
+                                    line.WriteLine(students[i].Name + " " + students[i].DateOfBirth);
+                                }
+
+                            }
+                        }                    
+                    }
+
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+                Console.WriteLine("Готово");
+            }
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Test
+{
+    public static void Main()
+    {
+        string path = @"C:\Users\amazi\OneDrive\Desktop\Task1\";
+
+        try
+        {
+            // Create the file, or overwrite if the file exists.
+            string add = path + "123.txt";
+            using (FileStream fs = File.Create(add))
+            {
+                byte[] info = new UTF8Encoding(true).GetBytes("This is some text in the file.");
+                // Add some information to the file.
+                fs.Write(info, 0, info.Length);
+            }
+
+            // Open the stream and read it back.
+            using (StreamReader sr = File.OpenText(add))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+    }
+}
