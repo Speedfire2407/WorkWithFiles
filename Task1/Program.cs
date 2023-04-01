c3650cc560cec3289603a0b57bcf9fbb79289b26
Speedfire2407 <Faunsatyr@yandex.ru>
2023-04-01 13:33:34 +0500

    Complete Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,30 +1,106 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 
-class Test
+class Program
 {
-    public static void Main()
+    static void Main(string[] args)
     {
-        string path = @"C:\\Users\\amazi\\OneDrive\\Desktop\\Task4\\test.txt";
-        if (!File.Exists(path))
+        Console.WriteLine("Укажите путь папки для отчистки");
+        string path = Console.ReadLine();
+        GetCatalogs(path);
+        Console.WriteLine("Готово");
+    }
+
+    static void GetFiles(string dirName)
+    {
+        
+        string dateNow = DateTime.Now.ToString();
+
+        if (Directory.Exists(dirName)) 
         {
-            // Create a file to write to.
-            using (StreamWriter sw = File.CreateText(path))
+            try
             {
-                sw.WriteLine("Hello");
-                sw.WriteLine("And");
-                sw.WriteLine("Welcome");
+                DirectoryInfo dir = new DirectoryInfo(dirName);
+                FileInfo[] files = dir.GetFiles();
+                foreach (FileInfo file in files) 
+                { 
+                    
+                    if (Directory.GetLastWriteTime(file.FullName) < DateTime.Now.AddMinutes(-30))
+                    {
+                        file.Delete();
+                        
+                    }
+                }
             }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+
         }
-        /*
-        // Open the file to read from.
-        using (StreamReader sr = File.OpenText(path))
+    }
+
+    static void GetCatalogs(string dirName)
+    {
+
+        string dateNow = DateTime.Now.ToString();
+
+        GetFiles(dirName);
+
+        if (Directory.Exists(dirName))
         {
-            string s = "";
-            while ((s = sr.ReadLine()) != null)
+            try
             {
-                Console.WriteLine(s);
+                DirectoryInfo dir = new DirectoryInfo(dirName);
+                DirectoryInfo[] dirs = dir.GetDirectories();
+                foreach (DirectoryInfo folder in dirs)
+                {
+                    if (Empty(folder))
+                    {
+                        if (File.GetLastWriteTime(folder.FullName) < DateTime.Now.AddMinutes(-30))
+                        {
+                            folder.Delete(true);
+                        }
+                    }
+                    else
+                    {
+                        GetFiles(folder.FullName);
+                        GetCatalogs(folder.FullName);
+                        if (Empty(folder))
+                        {
+                            if (folder.LastAccessTime < DateTime.Now.AddMinutes(-30))
+                            {
+                                folder.Delete(true);
+                            }
+                        }
+                    }                    
+                }
             }
-        }*/
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Название каталога указано не верно");
+        }
+    }
+
+    static bool Empty (DirectoryInfo directory)
+    {
+        DirectoryInfo[] dirs = directory.GetDirectories();
+        FileInfo[] files = directory.GetFiles();
+        if ((dirs.Length == 0) && (files.Length == 0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
     }
 }
